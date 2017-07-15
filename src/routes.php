281990a0399864207d85d0f38eac37bc38ef815d
william <goodwong@foxmail.com>
2017-07-15 19:40:05 +0800

    update api path

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,10 @@ Route::group([
     'namespace' => 'Goodwong\LaravelUser\Controllers',
 ], function () {
     Route::get('users/me', 'AuthController@show');
-    Route::post('users/me', 'AuthController@login');
-    Route::delete('users/me', 'AuthController@logout');
+    // Route::post('users/me', 'AuthController@register');
+    // Route::put('users/me', 'AuthController@update');
+    // Route::post('users/me/session', 'AuthController@login');
+    Route::delete('users/me/session', 'AuthController@logout');
+
+    // Route::resource('users', 'UserController');
 });
